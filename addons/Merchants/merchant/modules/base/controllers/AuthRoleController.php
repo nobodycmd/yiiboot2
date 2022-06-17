b099ab8f00d3b37c44a636d76f6f9e5f0213efdd
zhang <superboss01@163.com>
2022-06-17 23:03:55 +0800

    Update AuthRoleController.php

--- addons/Merchants/merchant/modules/base/controllers/AuthRoleController.php
+++ addons/Merchants/merchant/modules/base/controllers/AuthRoleController.php
@@ -35,7 +35,8 @@ class AuthRoleController extends BaseController
      *
      * @var bool
      */
-    public $sourceAuthChild = true;
+    //public $sourceAuthChild = true;
+    public $sourceAuthChild = false;
 
     /**
      * 渲染视图前缀
